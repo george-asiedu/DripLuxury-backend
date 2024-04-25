a9e076928758e1f4be0d6c4472bd2ffcf2e82850
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-04-25 14:18:36 +0000

    admin user schema created

---
+++ db/seeders/20240425085419-admin-user.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+require('dotenv').config({ path: `${process.cwd()}/.env` })
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    let password = process.env.ADMIN_PASSWORD
+    const hashPassword = bcrypt.hashSync(password, 10)
+    return queryInterface.bulkInsert('user', [
+      {
+        userType: 'Super Admin',
+        firstName: 'George',
+        lastName: 'Asiedu',
+        email: process.env.ADMIN_EMAIL,
+        password: hashPassword,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ])
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('user', { userType: 'Super Admin' }, {})
+  }
+}
