10ba65455ee5158f46ada6233ec871b4f796d001
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-05-20 09:39:43 +0000

    sequelize database connection created

--- config/database.js
+++ config/database.js
@@ -1,6 +1,23 @@
 const { Sequelize } = require("sequelize")
+
 const env = process.env.NODE_ENV || "development"
 const config = require('./config')
+
+if (!config[env]) {
+    console.error(`Error: Configuration for environment '${env}' not found.`)
+    process.exit(1)
+}
+
 const sequelize = new Sequelize(config[env])
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection to the database has been established successfully.')
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err)
+        process.exit(1)
+    })
 
 module.exports = sequelize
