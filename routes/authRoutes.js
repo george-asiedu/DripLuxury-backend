16cf6c60b70792d23c53eee4eaf0693f1b6487c6
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-05-20 09:47:33 +0000

    authRoutes file created to handle authentication routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router()
-const { signup, login } = require('../controllers/authController')
+const { signup,
+    login,
+    sendResetPasswordCode,
+    resetPasswordWithCode,
+    authentication
+} = require('../controllers/authController')
 
 router.route('/signup').post(signup)
 router.route('/login').post(login)
+router.route('/resetCode').post(sendResetPasswordCode)
+router.route('/resetPassword').post(resetPasswordWithCode)
+
+router.use(authentication)
 
 module.exports = router
