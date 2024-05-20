0c709153eb759b848f2751733c30406f9cb39dac
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-05-20 09:38:40 +0000

    app.js file updated to use routes and handle errors

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const authRoutes = require('./routes/authRoutes')
+const adminRoute = require('./routes/usersRoute')
+const productRoute = require('./routes/productRoutes')
 const AppError = require("./utils/appError");
 const catchErrorAsync = require("./utils/catchErrorAsync")
+const globalErrorHandler = require('./controllers/errorController')
 
 app.use(express.json())
 app.use(cors())
 
 app.use('/api/v1/auth', authRoutes)
+app.use('/api/users', adminRoute)
+app.use('/api/products', productRoute)
 
-app.use('*', catchErrorAsync(async (req, res, next) => {
+app.use('*', catchErrorAsync(async (req) => {
     throw new AppError(`Cannot find ${req.originalUrl}, on server port ${port}`, 401)
 }))
 
-const port = process.env.PORT || 3000
+app.use(globalErrorHandler)
+
+const port = process.env.PORT || 3001
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
