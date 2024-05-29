0cfef07150bbd060c21804acfaa5d04663a076ec
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-05-29 16:29:17 +0000

    product routes updated

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router()
 const { authentication, restrictTo } = require('../controllers/authController')
-const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } = require('../controllers/productController')
+const { createSingleProduct ,createManyProduct, getAllProducts, getProductById, updateProduct, deleteProduct } = require('../controllers/productController')
 
-router.route('/create').post(authentication, createProduct)
-router.route('/allProducts').get(getAllProducts)
+router.route('/create-many').post(authentication, createManyProduct)
+router.route('/create-single').post(authentication, createSingleProduct)
+router.route('/products').get(getAllProducts)
 router.route('/:id').get(authentication, getProductById)
 router.route('/:id').patch(authentication, updateProduct)
 router.route('/:id').delete(authentication, deleteProduct)
